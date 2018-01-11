85eba21e2060582a79f58424baa75c2e337bf124
s9369 <s9369@csx.com>
2018-01-11 16:21:34 -0500

    Commit DropdownDirective and corresponding html changes.

--- my-pantry/src/app/app.module.ts
+++ my-pantry/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { DropdownDirective } from './shared/dropdown.directive';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -23,7 +24,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeItemComponent,
     HeaderComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
